3caa7b52878c3b04c0a2be1ae2b1f132c1db59b1
hassanolatade <54205077+hassanolatade@users.noreply.github.com>
2020-11-22 15:17:32 +0100

    updated client-app with a shop page, sollection previewcomponent and collection and item component

--- client-app/src/App.js
+++ client-app/src/App.js
@@ -1,10 +1,15 @@
-import './App.css';
-import Homepage from './pages/hompage.component';
+import { Route, Switch } from "react-router-dom";
+import "./App.css";
+import Homepage from "./pages/homepage/hompage.component";
+import ShopPage from "./pages/shop/shop.component";
 
 function App() {
   return (
     <div className="">
-      <Homepage/>      
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route exact path="/shop" component={ShopPage} />
+      </Switch>
     </div>
   );
 }
